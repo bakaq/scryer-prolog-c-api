b4ba9e615a53796cb8a65feffe7293c70a99211f
bakaq <bakaq@bakaq.net>
2025-06-08 14:20:05 -0300

    Term methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::ffi::{CStr, CString, c_char};
+use std::ffi::{CStr, CString, c_char, c_double};
 
 #[repr(C)]
 pub enum Error {
@@ -215,3 +215,193 @@ pub unsafe extern "C" fn scryer_bindings_get(
 }
 
 // TODO: Iterator?
+
+// === Term methods ===
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_drop(term: Box<Term>) {
+    drop(term)
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_kind(term: &Term) -> TermKind {
+    match term.0 {
+        scryer_prolog::Term::Integer(_) => TermKind::Integer,
+        scryer_prolog::Term::Rational(_) => TermKind::Rational,
+        scryer_prolog::Term::Float(_) => TermKind::Float,
+        scryer_prolog::Term::Atom(_) => TermKind::Atom,
+        scryer_prolog::Term::String(_) => TermKind::String,
+        scryer_prolog::Term::List(_) => TermKind::List,
+        scryer_prolog::Term::Compound(_, _) => TermKind::Compound,
+        scryer_prolog::Term::Var(_) => TermKind::Variable,
+        _ => unreachable!(),
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_integer(
+    term: &Term,
+    big_integer: *mut *mut c_char,
+) -> Error {
+    let (error, big_integer_ptr) = if let scryer_prolog::Term::Integer(big_int) = &term.0 {
+        (
+            Error::Success,
+            CString::new(big_int.to_string()).unwrap().into_raw(),
+        )
+    } else {
+        (Error::Error, std::ptr::null_mut())
+    };
+
+    unsafe { *big_integer = big_integer_ptr };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_float(
+    term: &Term,
+    scryer_float: *mut c_double,
+) -> Error {
+    let (error, scryer_float_val) = if let scryer_prolog::Term::Float(float) = &term.0 {
+        (Error::Success, *float)
+    } else {
+        (Error::Error, 0.0)
+    };
+
+    unsafe { *scryer_float = scryer_float_val };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_rational(
+    term: &Term,
+    numerator: *mut *mut c_char,
+    denominator: *mut *mut c_char,
+) -> Error {
+    let (error, numerator_ptr, denominator_ptr) =
+        if let scryer_prolog::Term::Rational(rational) = &term.0 {
+            let (num, den) = rational.clone().into_parts();
+            (
+                Error::Success,
+                CString::new(num.to_string()).unwrap().into_raw(),
+                CString::new(den.to_string()).unwrap().into_raw(),
+            )
+        } else {
+            (Error::Error, std::ptr::null_mut(), std::ptr::null_mut())
+        };
+
+    unsafe { *numerator = numerator_ptr };
+    unsafe { *denominator = denominator_ptr };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_atom(term: &Term, atom: *mut *mut c_char) -> Error {
+    let (error, atom_ptr) = if let scryer_prolog::Term::Atom(a) = &term.0 {
+        (Error::Success, CString::new(a.clone()).unwrap().into_raw())
+    } else {
+        (Error::Error, std::ptr::null_mut())
+    };
+
+    unsafe { *atom = atom_ptr };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_string(term: &Term, string: *mut *mut c_char) -> Error {
+    let (error, string_ptr) = if let scryer_prolog::Term::String(s) = &term.0 {
+        (Error::Success, CString::new(s.clone()).unwrap().into_raw())
+    } else {
+        (Error::Error, std::ptr::null_mut())
+    };
+
+    unsafe { *string = string_ptr };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_list(
+    term: &Term,
+    term_list: *mut *mut *mut Term,
+    len: *mut usize,
+) -> Error {
+    let (error, term_list_ptr, term_list_len) = if let scryer_prolog::Term::List(l) = &term.0 {
+        let mut term_list_vec: Vec<*mut Term> = l
+            .iter()
+            .map(|t| Box::into_raw(Box::new(Term(t.clone()))))
+            .collect();
+
+        term_list_vec.shrink_to_fit();
+        assert_eq!(term_list_vec.len(), term_list_vec.capacity());
+
+        let list_ptr = term_list_vec.as_mut_ptr();
+        let len = term_list_vec.len();
+
+        std::mem::forget(term_list_vec);
+
+        (Error::Success, list_ptr, len)
+    } else {
+        (Error::Error, std::ptr::null_mut(), 0)
+    };
+
+    unsafe { *term_list = term_list_ptr };
+    unsafe { *len = term_list_len };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_compound(
+    term: &Term,
+    functor: *mut *mut c_char,
+    args: *mut *mut *mut Term,
+    len: *mut usize,
+) -> Error {
+    let (error, functor_ptr, args_ptr, args_len) =
+        if let scryer_prolog::Term::Compound(f, args) = &term.0 {
+            let functor_ptr = CString::new(f.clone()).unwrap().into_raw();
+
+            let mut args_vec: Vec<*mut Term> = args
+                .iter()
+                .map(|t| Box::into_raw(Box::new(Term(t.clone()))))
+                .collect();
+
+            args_vec.shrink_to_fit();
+            assert_eq!(args_vec.len(), args_vec.capacity());
+
+            let list_ptr = args_vec.as_mut_ptr();
+            let len = args_vec.len();
+
+            std::mem::forget(args_vec);
+
+            (Error::Success, functor_ptr, list_ptr, len)
+        } else {
+            (Error::Error, std::ptr::null_mut(), std::ptr::null_mut(), 0)
+        };
+
+    unsafe { *functor = functor_ptr };
+    unsafe { *args = args_ptr };
+    unsafe { *len = args_len };
+
+    error
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn scryer_term_unwrap_variable(
+    term: &Term,
+    variable: *mut *mut c_char,
+) -> Error {
+    let (error, variable_ptr) = if let scryer_prolog::Term::Var(v) = &term.0 {
+        (Error::Success, CString::new(v.clone()).unwrap().into_raw())
+    } else {
+        (Error::Error, std::ptr::null_mut())
+    };
+
+    unsafe { *variable = variable_ptr };
+
+    error
+}
